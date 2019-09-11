c00fe447fd933ddb00b68d0e47393aa8376a912e
vagrant <vagrant@homestead>
2019-09-11 03:31:29 +0000

    support

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,23 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('rut')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('level');
+            $table->string('fav_drink');
+            $table->integer('dd');
+            $table->integer('mm');
+            $table->integer('yy');
+            $table->string('fav_cake');
+            $table->integer('points');
+            $table->string('ocupation');
+            $table->integer('fika10');
+            $table->integer('fika15');
+            $table->string('region');
+            $table->string('city');
             $table->rememberToken();
             $table->timestamps();
         });
