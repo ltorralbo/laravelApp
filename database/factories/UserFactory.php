bb09268fb402e930075b38c7d840f5121c11992f
vagrant <vagrant@homestead>
2019-09-12 06:15:12 +0000

    seeds

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,6 +19,21 @@ use Faker\Generator as Faker;
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
+        'last_name' => $faker->name,
+        'rut' => rand(14550505, 1823454323),
+        'yy' => rand(1976, 1999),
+        'mm' => rand(1, 12),
+        'dd' => rand(1, 30),
+        'dd' => rand(1, 30),
+        'fika15' => rand(1, 2),
+        'fika10' => rand(1, 2),
+        'points' => rand(0, 83232),
+        'region' => $faker->randomElement(['Valparaíso', 'Región metropolitana']),
+        'city' => $faker->randomElement(['Valparaíso', 'Viña del mar', 'quilpué']),
+        'ocupation' => $faker->randomElement(['estudiante', 'trabajador']),
+        'level' => $faker->randomElement(['basica', 'plata', 'oro']),
+        'fav_drink' => $faker->randomElement(['Cubano', 'Americano', 'Capuccino', 'Mocacchino']),
+        'fav_cake' => $faker->randomElement(['Tarta manjar', 'la bombe', 'Chessecake Maracuyá', 'Chessecake Frambuesa', 'Eclair']),
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
